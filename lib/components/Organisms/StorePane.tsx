eed74707090cd9d7ce4d3b75eb929c1542b3f0e1
TAKAYAMAN2180 <sirokurodog_2180@yahoo.co.jp>
2023-06-19 01:25:51 +0900

    add colored underline for congestion level

--- lib/components/Organisms/StorePane.tsx
+++ lib/components/Organisms/StorePane.tsx
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 import {Box, Grow} from "@mui/material";
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import Image from "next/image";
 import CancelBtn from "../Atom/Button/CancelBtn";
-import StoresInfoType from "../../type/StoresInfoType";
-import storesInfoData from "../../../public/data/storesInfoData.json"
-import CongestionDataType from "../../type/CongestionDataType";
 import StorePaneType from "../../type/StorePaneType";
+import CongestionSentence from "../Atom/CongestionSentence";
 
 type Props = {
     storePaneData: StorePaneType | null;
@@ -45,7 +43,6 @@ const StoreDescDiv = ({title, desc}: StoreDescProps) => {
 }
 
 const StorePane = ({storePaneData, handleClosed}: Props) => {
-    //
     const [isStorePaneVisible, setIsStorePaneVisible] = useState<boolean>(false);
 
     useEffect(() => {
@@ -57,55 +54,45 @@ const StorePane = ({storePaneData, handleClosed}: Props) => {
     }, [storePaneData]);
 
     return (
-            <>
+        <>
             {storePaneData != null ?
                 <Grow in={isStorePaneVisible}>
-                <div style={{
-                    position: "fixed",
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    zIndex: 400,
-                }}>
-                    <FixedBox sx={{boxShadow: 3}} borderRadius={3}>
-                        <CancelBtn onClick={()=>{handleClosed()}}/>
+                    <div style={{
+                        position: "fixed",
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        zIndex: 400,
+                    }}>
+                        <FixedBox sx={{boxShadow: 3}} borderRadius={3}>
+                            <CancelBtn onClick={() => {
+                                handleClosed()
+                            }}/>
 
-                        <div style={{textAlign: "center", pointerEvents: "none"}}>
-                            <Image src={`/img/marks/congestion${storePaneData?.congestionLevel}.webp`}
-                                   alt={"congestion_level"} height={32} width={32}
-                                   style={{position: "relative", top: 6}}/>
-                            <span style={{fontSize: "1.7rem", marginLeft: "1rem", fontWeight: "bold"}}>
-                         {(() => {
-                             switch (storePaneData?.congestionLevel) {
-                                 case 0:
-                                     return "中止しています";
-                                 case 1:
-                                     return "空いています";
-                                 case 2:
-                                     return "少し混雑しています";
-                                 case 3:
-                                     return "混雑しています";
-                             }
-                         })()}
-                            </span>
-                        </div>
-                        <StoreDescDiv title={"店舗名"} desc={storePaneData?.storeName}/>
-                        <StoreDescDiv title={"販売品目"} desc={storePaneData?.food}/>
-                        <StoreDescDiv title={"区画番号"} desc={storePaneData?.areaNum}/>
-                        <div style={{
-                            textAlign: "center",
-                            position: "absolute",
-                            right: 15,
-                            top: "75%",
-                            color: "#696969"
-                        }}>
-                            <div style={{fontSize: 10, marginBottom: -5}}>更新時間</div>
-                            <div>{storePaneData?.updateTime}</div>
-                        </div>
-                    </FixedBox>
-                </div></Grow>:<></>
+                            <div style={{textAlign: "center", pointerEvents: "none"}}>
+                                <Image src={`/img/marks/congestion${storePaneData?.congestionLevel}.webp`}
+                                       alt={"congestion_level"} height={32} width={32}
+                                       style={{position: "relative", top: 6}}/>
+                                <CongestionSentence congestionLevel={storePaneData?.congestionLevel}/>
+                            </div>
+                            <StoreDescDiv title={"店舗名"} desc={storePaneData?.storeName}/>
+                            <StoreDescDiv title={"販売品目"} desc={storePaneData?.food}/>
+                            <StoreDescDiv title={"区画番号"} desc={storePaneData?.areaNum}/>
+                            <div style={{
+                                textAlign: "center",
+                                position: "absolute",
+                                right: 15,
+                                top: "75%",
+                                color: "#696969"
+                            }}>
+                                <div style={{fontSize: 10, marginBottom: -5}}>更新時間</div>
+                                <div>{storePaneData?.updateTime}</div>
+                            </div>
+                        </FixedBox>
+                    </div>
+                </Grow> : <></>
             }
-            </>
+        </>
     )
 }
 
