a408f3b18b3577cf54db7f5379ea757457e4a672
TAKAYAMAN2180 <sirokurodog_2180@yahoo.co.jp>
2023-07-13 01:26:14 +0900

    change Amplify-configure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,13 @@ import {Amplify} from "aws-amplify";
 import awsmobile from "@/src/aws-exports";
 
 
-Amplify.configure(awsmobile)
+Amplify.configure({
+    "aws_project_region": "ap-northeast-1",
+    "aws_appsync_graphqlEndpoint": "https://u7s7l5cvmrcthlqj6zgwnm6toa.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": process.env.COGNITO_API_KEY
+})
 
 
 
