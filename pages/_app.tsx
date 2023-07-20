c7bb1ee975ebf03121529a1df8738a8b54384edc
TAKAYAMAN2180 <sirokurodog_2180@yahoo.co.jp>
2023-07-20 19:29:44 +0900

    delete unused statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import type {AppProps} from 'next/app'
-import React, {createContext, useState} from "react";
+import React from "react";
 import {CssBaseline} from "@mui/material";
-import HideScrollBarDiv from "../lib/style/HideScrollBarDiv";
-import CongestionDataType from "../lib/type/CongestionDataType";
-import congestionDataSample from "../public/data/test/congestionDataSample.json";
 import {SessionProvider} from "next-auth/react";
-import {Amplify} from "aws-amplify";
 import HEAD from "next/head";
-import GoogleAnalytics from "@/lib/GoogleAnalytics";
+
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
