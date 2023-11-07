54a5925bddc687dfa10c00b8bac5a2709d1a6ab7
takayaman2180 <beruberudog.27@gmail.com>
2023-11-07 17:44:31 +0900

    update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import type {AppProps} from 'next/app'
+import {AppProps} from 'next/app'
 import React from "react";
 import {CssBaseline} from "@mui/material";
 import {SessionProvider} from "next-auth/react";
