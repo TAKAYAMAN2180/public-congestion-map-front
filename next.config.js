f6d6477a6a756baf28c876e11fde672baa6171b3
takayaman2180 <beruberudog.27@gmail.com>
2023-11-24 17:25:32 +0900

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
 module.exports = nextConfig;
 
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        and: [/\.(js|ts)x?$/],
-      },
-      use: ["@svgr/webpack"],
-    });
+    images: {
+        disableStaticImages: true
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+                and: [/\.(js|ts)x?$/],
+            },
+            use: ["@svgr/webpack"],
+        });
 
-    // HTMLファイルのローダー設定
-    config.module.rules.push({
-      test: /\.html$/,
-      use: "html-loader",
-    });
+        // HTMLファイルのローダー設定
+        config.module.rules.push({
+            test: /\.html$/,
+            use: "html-loader",
+        });
 
-    return config;
-  },
+        return config;
+    },
 };
